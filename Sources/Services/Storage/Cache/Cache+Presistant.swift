5e353917a71d96c936afd080618b79ce8ae047a0
Maxim Krouk <m.krouk@vironit.co.uk>
2019-08-06 10:34:25 +0300

    Storage: keychain refactored, persistent cache thread safety added.

--- Sources/Services/Storage/Cache/Cache+Presistant.swift
+++ Sources/Services/Storage/Cache/Cache+Presistant.swift
@@ -10,6 +10,7 @@ import Foundation
 extension Storage.Cache {
     
     class Presistant: StorageManager {
+        private let lock = NSLock()
         private let fileManager = FileManager.default
         public var directory: URL {
             cacheDirectory.appendingPathComponent("./\(Bundle.main.bundleIdentifier.unwrap(default: "ConvenienceTempDir"))")
@@ -22,41 +23,49 @@ extension Storage.Cache {
         }
         
         public func data(forKey key: String) -> AFResult<Data> {
-            guard let data = fileManager.contents(atPath: url(for: key).path) else {
-                return .failure(PlainError("No data found for key: [\(key)]"))
+            lock.execute {
+                guard let data = fileManager.contents(atPath: url(for: key).path) else {
+                    return .failure(PlainError("No data found for key: [\(key)]"))
+                }
+                return .success(data)
             }
-            return .success(data)
         }
 
         @discardableResult
         public func save(data: Data, forKey key: String) -> AFResult<Void> {
-            let path = url(for: key).path
-            delete(key: key)
-            let result = fileManager.createFile(atPath: path, contents: data, attributes: nil)
-            return result ?
-                .success(()) :
-                .failure(PlainError("File already exists for key: [\(key)]. Failed to rewrite."))
+            lock.execute {
+                let path = url(for: key).path
+                delete(key: key)
+                let result = fileManager.createFile(atPath: path, contents: data, attributes: nil)
+                return result ?
+                    .success(()) :
+                    .failure(PlainError("File already exists for key: [\(key)]. Failed to rewrite."))
+            }
         }
         
         @discardableResult
         public func delete(key: String) -> AFResult<Void> {
-            do {
-                let path = url(for: key).path
-                if fileManager.fileExists(atPath: path) { try fileManager.removeItem(atPath: path) }
-                return .success(())
-            } catch {
-                return .failure(error)
+            lock.execute {
+                do {
+                    let path = url(for: key).path
+                    if fileManager.fileExists(atPath: path) { try fileManager.removeItem(atPath: path) }
+                    return .success(())
+                } catch {
+                    return .failure(error)
+                }
             }
         }
 
         @discardableResult
         public func clear() -> AFResult<Void> {
-            do {
-                let contents = try fileManager.contentsOfDirectory(atPath: directory.path)
-                contents.forEach { try? fileManager.removeItem(atPath: $0) }
-                return .success(())
-            } catch {
-                return .failure(error)
+            lock.execute {
+                do {
+                    let contents = try fileManager.contentsOfDirectory(atPath: directory.path)
+                    contents.forEach { try? fileManager.removeItem(atPath: $0) }
+                    return .success(())
+                } catch {
+                    return .failure(error)
+                }
             }
         }
     }
