c8f6f187c882952b1fd49ba07e668947bc7bc8df
Maxim Krouk <id.maximkrouk@gmail.com>
2019-08-10 13:16:40 +0300

    Error: Collect and Fallback is now functions.

--- Sources/Services/ErrorHandling/Collect/Collect.swift
+++ Sources/Services/ErrorHandling/Collect/Collect.swift
@@ -7,28 +7,23 @@
 
 import Foundation
 
-public struct Collect {
-    let result: AFResult<Void>
-    init(@Fallback.Builer closure: Fallback.Builer.Closure) {
-        do { result = .success(try closure()()) }
-        catch { result = .failure(error) }
-    }
+public func collect(@CollectBuiler closure: CollectBuiler.Closure) -> AFResult<Void> {
+    do { return .success(try closure()()) }
+    catch { return .failure(error) }
 }
 
-public extension Collect {
-    @_functionBuilder
-    struct Builer {
-        typealias Closure = () -> Block
-        typealias Block = () throws -> Void
-        static func buildBlock(_ items: Block...) -> Block {
-            return {
-                var errors = Errors()
-                items.forEach {
-                    do { try $0() }
-                    catch { errors.append(error) }
-                }
-                guard errors.isEmpty else { throw ErrorBag(errors) }
+@_functionBuilder
+public struct CollectBuiler {
+    public typealias Closure = () -> Block
+    public typealias Block = () throws -> Void
+    public static func buildBlock(_ items: Block...) -> Block {
+        return {
+            var errors = Errors()
+            items.forEach {
+                do { try $0() }
+                catch { errors.append(error) }
             }
+            guard errors.isEmpty else { throw ErrorBag(errors) }
         }
     }
 }
--- Sources/Services/ErrorHandling/Fallback/Fallback.swift
+++ Sources/Services/ErrorHandling/Fallback/Fallback.swift
@@ -7,28 +7,23 @@
 
 import Foundation
 
-public struct Fallback {
-    let result: AFResult<Void>
-    init(@Fallback.Builer closure: Fallback.Builer.Closure) {
-        do { result = .success(try closure()()) }
-        catch { result = .failure(error) }
-    }
+public func fallback(@Fallback.Builer closure: Fallback.Builer.Closure) -> AFResult<Void> {
+    do { return .success(try closure()()) }
+    catch { return .failure(error) }
 }
 
-public extension Fallback {
-    @_functionBuilder
-    struct Builer {
-        typealias Closure = () -> Block
-        typealias Block = () throws -> Void
-        static func buildBlock(_ items: Block...) -> Block {
-            return {
-                do {
-                    try items.forEach {
-                        do { try $0() }
-                        catch { throw error }
-                    }
-                } catch { throw error }
-            }
+public struct FallbackBuiler {
+    typealias Closure = () -> Block
+    typealias Block = () throws -> Void
+    static func buildBlock(_ items: Block...) -> Block {
+        return {
+            do {
+                try items.forEach {
+                    do { try $0() }
+                    catch { throw error }
+                }
+            } catch { throw error }
         }
     }
+    
 }
