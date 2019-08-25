54a72596d5799b4a4c1290520575b0d629499771
Maxim Krouk <id.maximkrouk@gmail.com>
2019-08-25 17:53:49 +0300

    Project: build version updated, modification functions added.

---
+++ Sources/Other/Modification.swift
@@ -0,0 +1,34 @@
+//
+//  Modification.swift
+//  Convenience
+//
+//  Created by Maxim Krouk on 8/25/19.
+//
+
+import Foundation
+
+/// Modifies the instance and returns the result.
+///
+/// If an object is an instance of a reference type, than the object will be modified and than returned.
+/// Otherwise the copy of an object will be created, modified and returned.
+///
+/// - Parameter object: The instance.
+/// - Parameter closure: Modification.
+/// - Returns: modified object.
+public func modification<T>(of object: T, closure: (inout T) -> Void) -> T {
+    var object = object
+    closure(&object)
+    return object
+}
+
+/// Performs specified modification on any instance.
+///
+/// - Parameter object: The instance.
+/// - Parameter closure: Modification.
+public func modify<T>(object: inout T, closure: (inout T) -> Void) { closure(&object) }
+
+/// Performs specified modification on the instance of a class.
+///
+/// - Parameter object: Instance of a class.
+/// - Parameter closure: Modification.
+public func modify<T: AnyObject>(object: T, closure: (T) -> Void) { closure(object) }
