a684e3073431216ecfa60a55b04856d2dc0ddab8
Maxim Krouk <m.krouk@vironit.co.uk>
2019-08-26 18:28:30 +0300

    Cast: cast functions added.

---
+++ Sources/Other/Cast.swift
@@ -0,0 +1,20 @@
+//
+//  Cast.swift
+//  Convenience
+//
+//  Created by Maxim Krouk on 8/26/19.
+//
+
+import Foundation
+
+public func usnafeCast<T>(_ object: Any, to type: T.Type) -> T! { cast(object, to: type) }
+
+public func cast<T>(_ object: Any, default defaultValue: @autoclosure () -> T) -> T {
+    cast(object, to: T.self, default: defaultValue())
+}
+
+public func cast<T>(_ object: Any, to type: T.Type, default defaultValue: @autoclosure () -> T) -> T {
+    cast(object, to: type).unwrap(default: defaultValue())
+}
+
+public func cast<T>(_ object: Any, to type: T.Type) -> T? { object as? T }
