0de35759f3905ff363023884289c7f3a3f16178e
Maxim Krouk <id.maximkrouk@gmail.com>
2019-08-28 00:43:47 +0300

    Bugfix [critical]: access modifiers issue fixed.

--- Sources/Services/Storage/Cache/Cache+Optimized.swift
+++ Sources/Services/Storage/Cache/Cache+Optimized.swift
@@ -53,8 +53,8 @@ private extension Storage.Cache.Optimized {
     }
 }
 
-private extension Storage.Cache.Optimized: Codable where Key: Codable, Value: Codable {
-    convenience public init(from decoder: Decoder) throws {
+extension Storage.Cache.Optimized: Codable where Key: Codable, Value: Codable {
+    convenience init(from decoder: Decoder) throws {
         self.init()
 
         let container = try decoder.singleValueContainer()
@@ -62,7 +62,7 @@ private extension Storage.Cache.Optimized: Codable where Key: Codable, Value: Co
         entries.forEach(insert)
     }
 
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(keyTracker.keys.compactMap(entry))
     }
