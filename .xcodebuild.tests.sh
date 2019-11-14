46ae6d012ea353391d388cee3a525621d8206670
Maxim Krouk <40476363+maximkrouk@users.noreply.github.com>
2019-11-14 14:39:35 +0300

    Update .xcodebuild.tests.sh

--- .xcodebuild.tests.sh
+++ .xcodebuild.tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+swift package generate-xcodeproject
 xcodebuild \
-    -workspace Example/Convenience.xcworkspace \
-    -scheme Convenience-Example \
+    -workspace Convenience.xcodeproject \
+    -scheme Convenience-Package \
     -destination "platform=iOS Simulator,OS=13.0,name=iPhone Xs" \
     clean test | xcpretty
